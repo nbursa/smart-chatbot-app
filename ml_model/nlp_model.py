7780cc43affa1c29669a1a73d28cee8be7180faa
Nenad <nbursa@gmail.com>
2024-07-03 14:34:05 +0200

    Model: cleanup

--- ml_model/nlp_model.py
+++ ml_model/nlp_model.py
@@ -2,13 +2,10 @@ import nltk
 import spacy
 from flask import Flask, request, jsonify
 
-# Initialize Flask app
 app = Flask(__name__)
 
-# Setup spaCy model
 nlp = spacy.load("en_core_web_sm")
 
-# Endpoint for processing text
 @app.route('/process', methods=['POST'])
 def process_text():
     data = request.json
@@ -16,11 +13,9 @@ def process_text():
     if not text:
         return jsonify({'error': 'No text provided'}), 400
 
-    # Tokenization using NLTK
     nltk_tokens = nltk.word_tokenize(text)
-    processed_text = ' '.join(nltk_tokens)  # Join tokens into a single string
+    processed_text = ' '.join(nltk_tokens)
 
-    # Named Entity Recognition using spaCy
     spacy_doc = nlp(text)
     entities = [(ent.text, ent.label_) for ent in spacy_doc.ents]
 
